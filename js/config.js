a0d1fe22dde60df317576ef6277d2a1c2efe4d40
LucCo99 <coluc8440@gmail.com>
2023-04-30 23:02:28 +1000

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-2_GEYP2uYRx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3eroh4orrev1c1hbbgl7v617bd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-2_WmA1Anzmr', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7mqv99i33qjj52m1g09bq2vhv0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
