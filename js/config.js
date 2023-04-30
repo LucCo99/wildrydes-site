f461c2fd518e026e9197c82043d555537dfd4538
LucCo99 <coluc8440@gmail.com>
2023-04-30 21:31:05 +1000

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_mWtnz8J6Y', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6pg1l2i49t14dmtvj86d2ueiu1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
